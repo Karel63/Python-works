ac35eacb1ce4d553b306251bd91b18fcd37b84fc
Karel63 <76937128+Karel63@users.noreply.github.com>
2022-01-25 11:24:56 +0100

    Create BitovyPosuv.py

---
+++ BitovyPosuv.py
@@ -0,0 +1,10 @@
+#Zde se zadají 3 hodnoty
+X = [input(), input(), input()]
+
+print("Zadané čísla:", X[0], X[1], X[2])
+
+#V\píše se poze první a druhá hodnota
+Posun = [X[0], X[1]] 
+
+#Vypíše se posun
+print("Posun", Posun)
